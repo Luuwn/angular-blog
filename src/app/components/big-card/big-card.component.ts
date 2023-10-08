1d9c38350b8683b8fef876ed15d760445f6e451b
Luan Costa <112595656+luuwn@users.noreply.github.com>
2023-10-08 21:50:56 +0000

    update: decoupling card components

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
   templateUrl: './big-card.component.html',
   styleUrls: ['./big-card.component.css']
 })
-export class BigCardComponent {
+export class BigCardComponent{
+  
+  @Input()
+  photoCover:string = "https://camo.githubusercontent.com/b7b7dca15c743879821e7cfc14e8034ecee3588e221de0a6f436423e304d95f5/68747470733a2f2f7a7562652e696f2f66696c65732f706f722d756d612d626f612d63617573612f33363664616462316461323032353338616531333332396261333464393030362d696d6167652e706e67"
+  @Input()
+  cardDate:string = "Janeiro 01, 2023"
+  @Input()
+  cardTitle:string = "Titulo Modelo: Modelos"
+  @Input()
+  cardDescription:string = "Descrição Modelo, Descrição Modelo, Descrição Modelo, Descrição Modelo, Descrição Modelo, Descrição Modelo, Descrição Modelo."  
 
 }
